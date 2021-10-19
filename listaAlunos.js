03b3475d500860bfff8fb55b94c2c01d8e955210
Nickson Iduino <nickson.ioc@gmail.com>
2021-10-18 21:31:06 -0300

    commit da atividade do encontro remoto 2

---
+++ listaAlunos.js
@@ -0,0 +1,12 @@
+let alunos = 6;
+
+
+for (let x=0; x<alunos; x++) {
+    if (x%2==0){
+        console.log("Par");
+    } else if(x==0){
+        console.log("ZERO");
+    } else {
+        console.log("Ímpar");
+    }
+}
